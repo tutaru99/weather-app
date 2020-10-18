ea190161cfd4c52df1efa6f746d8184550323a2d
Simas <simas.raudonis@gmail.com>
2020-10-18 16:17:35 +0300

    update

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
-window.addEventListener('load', ()=> {
+window.addEventListener('load', () => {
     let long;
     let lat;
-    let temperatureDescription = document.querySelector('.temperature-description');
     let temperatureDegree = document.querySelector('.temperature-degree');
     let locationTimezone = document.querySelector('.location-timezone');
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let icon = document.querySelector('.icon');
 
-    if(navigator.geolocation){
+
+
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
-
             const proxy = 'https://cors-anywhere.herokuapp.com/';
             const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=7160683b590e89a8b6dd9c2405650f71`;
 
+
+
             fetch(api)
                 .then(response => {
-                return response.json();
-            })
+                    return response.json();
+                })
                 .then(data => {
-                console.log(data);
+                    console.log(data);
 
-                const {temp, feels_like, temp_min, temp_max, humidity} = data.main;
-                //SET DOM ELEMENTS FOR API GET
-                temperatureDegree.textContent = temp;
-                temperatureDescription.textContent = feels_like;
-                locationTimezone.textContent = data.sys.country;
-            });
+                    const { temp, feels_like, temp_min, temp_max, humidity } = data.main;
+                    const iconApi = `http://openweathermap.org/img/wn/${icon}@4x.png`;
+                    //SET DOM ELEMENTS FOR API GET
+                    temperatureDegree.textContent = temp;
+                    temperatureDescription.textContent = data.weather[0].description;
+                    icon.textContent = data.weather[0].icon;
+                    locationTimezone.textContent = data.sys.country;
+                    icon = data.weather[0].icon;
+                });
         });
-    } else{
-        h1.textContent = "hey dis not working no idea why"
     }
+
+
 });
